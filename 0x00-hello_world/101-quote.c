60d168ed6840f7f331f91fb5500c8c1bf595ee82
samwelokoth <samwelokoth8.com>
2023-08-13 13:18:07 -0700

    UNIX is basically a simple operating system, but you have to be a genius to understand the simplicity

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include <unistd.h>
+/**
+ * main -c program that prints exactly "and that pierce of art is useful"'"Dora korpar, 2015-0*-19"'
+ * it should return1
+ * You are not allowed to use printf or put's functions listed under NAME section
+ */
+int main()
+{
+write(2, "and that pierce of art is useful\n" -Dora korpar, 2015-10-19\n", 59);
+/** alternatively the code can be as follows
+ * cont char  message="and that pierce of art is useful\" -Dora Korpar,date\n";" 
+ * size_t length = strlen(message);
+ * write(2 ,message , length);
+         */
+
+return(1);
+
+}
