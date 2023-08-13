d6fea0e330f0e8f07df69caf09c0299961d5b201
samwelokoth <samwelokoth8.com>
2023-08-13 05:20:32 -0700

    Hello , puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,12 @@
 /*
 *main -Prints the phrase ""Programming is like building a multilingual puzzle"
 *Return 0: if exited properly else non-zero
-*/
+*@brief Entry point of the program.
+*
+*
+* This function is the entry pointof the program and is 
+*automatically called when the program is executed.
+* */
 int main()
 {
 puts ("\"Programming is like building a multilingual puzzle\n");
