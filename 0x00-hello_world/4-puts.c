7372b594b4e0b6465bc83d3ee68ea3f6fec9ee8a
samwelokoth <samwelokoth8.com>
2023-08-13 05:13:24 -0700

    Hello , puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/*
+*main -Prints the phrase ""Programming is like building a multilingual puzzle"
+*Return 0: if exited properly else non-zero
+*/
+int main()
+{
+puts ("\"Programming is like building a multilingual puzzle\n");
+return (0);
+}
