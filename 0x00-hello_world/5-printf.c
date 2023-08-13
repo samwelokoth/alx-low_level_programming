2bf792881e8563c63ba22391de03e5f16d2ed413
samwelokoth <samwelokoth8.com>
2023-08-13 06:36:45 -0700

    Hello,printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,22 +1,18 @@
 #include <stdio.h>
-/*@brief _Introduction part 
- *c program that prints exactly with proper grammar, but the outcome is a piece of art,
 
+/**
+ * main - Entry point of the program.
  *
-    Use the function printf
-    You are not allowed to use the function puts
-    Your program should return 0
-    Your program should compile without warning when using the -Wall gcc option
-
+ * Description: This program prints a specific text to the standard output.
+ * Use the function printf to achieve this.
  *
- *
+ * Return: Always 0 (Success).
  */
-int main()
+int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n");
-/*main part of the program*/
+    printf("with proper grammar, but the outcome is a piece of art,\n");
+    /* The main part of the program */
 
-return (0);/*if success*/
-
-
+    return (0); /* If success */
 }
+
