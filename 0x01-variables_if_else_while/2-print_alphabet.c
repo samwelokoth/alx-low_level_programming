30f1377c055140116436aa49ba0fc2ccb8117c7f
samwelokoth <samwelokoth8.com>
2023-08-13 22:48:10 -0700

    I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,24 +1,19 @@
 #include <stdio.h>
 
 /**
- * Main -Entry part of the program
- * @brief- write a program that prints the alphabet
- * followed by a new line
+ * main - Entry point of the program.
+ *
+ * Description: This program prints the lowercase alphabet characters.
+ *
+ * Return: Always 0 (Success).
  */
-
 int main(void)
 {
+    char letter;
 
-/*
- *main -prints the alphabets
- *Return : Always 0 (success)
- * you can only use putchar
- * All your code should be in the main function
- * you can only use putchar twice in your code
-*/
-char letter;
-for (letter  = 'a'; letter <= 'z'; letter++)
-putchar(letter);
-putchar('\n');
-return (0);
+    for (letter = 'a'; letter <= 'z'; letter++)
+        putchar(letter), putchar('\n');
+
+    return (0);
 }
+
