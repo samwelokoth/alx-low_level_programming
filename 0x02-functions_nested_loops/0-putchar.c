06c5d64e09a995114f191ead0f9b42c871e489b4
samwelokoth <samwelokoth8.com>
2023-08-15 12:28:42 -0700

    Tasks

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/*
+ * Main-Entry point of the program
+ * Return : Always 0 if success
+ * Description : program that prints _putchar followed by a new line
+ *
+ */
+
+int main(void)
+{
+printf("_putchar\n");
+return (0);
+}
+
