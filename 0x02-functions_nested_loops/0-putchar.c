6c83387b71a202fb9d9586a47e7d45354d78c06f
samwelokoth <samwelokoth8.com>
2023-08-16 11:18:27 -0700

    Tasks

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,16 +1,19 @@
-#include <stdio.h>
-
-/*
- * Main-Entry point of the program
- * Return : Always 0 if success
- * Description : program that prints _putchar followed by a new line
- *
- */
-
+#include "holberton.h"
+/**
+*main - prints Holberton
+*Return: 0
+*/
 int main(void)
 {
-printf("_putchar\n");
-putchar('\n');
-return (0);
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
+	_putchar('\n');
+	return (0);
 }
-
