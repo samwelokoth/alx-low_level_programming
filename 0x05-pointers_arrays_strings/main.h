413812d905e61190cc02b0f6e948c8e7c7114983
samwelokoth <samwelokoth8.com>
2023-08-22 13:53:24 -0700

    I do not fear computers. I fear the lack of them

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -14,6 +14,7 @@ void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
 void print_number(int n);
+void _puts(char *str);
 int _putchar(char);
 int _isupper(int);
 int _isdigit(int);
