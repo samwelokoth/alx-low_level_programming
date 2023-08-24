46773fab9703b5f6958ef3f72bd46ab30997234f
samwelokoth <samwelokoth8@gmail.com>
2023-08-24 18:33:38 +0300

    strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,32 +1,28 @@
-#include <stdio.h>
+
 #include "main.h"
-
-/*main -Entry point to the program
-Write a program that copies a string 
-the Program should work exactly like the strcpy
-strncpy used to copy strings 
-*/
-
+/**
+ * _strncpy - copy a string
+ * @dest: input value
+ * @src: input value
+ * @n: input value
+ *
+ * Return: dest
+ */
 char *_strncpy(char *dest, char *src, int n)
-
-
 {
-	int i;
+	int j;
 
-	i = 0;
-
-	while (src[i] != '\0' && i < n)
+	j = 0;
+	while (j < n && src[j] != '\0')
 	{
-		dest[i] = src[i];
-		i++;
+		dest[j] = src[j];
+		j++;
 	}
-
-	while (i < n)
+	while (j < n)
 	{
-		dest[i] = '\0';
-		i++;
+		dest[j] = '\0';
+		j++;
 	}
 
 	return (dest);
 }
-
