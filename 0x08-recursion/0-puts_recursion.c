a0d3dc56d791844b24c30190baa765329ca7b398
samwelokoth <samwelokoth8@gmail.com>
2023-08-30 21:51:34 +0300

    She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,25 +1,18 @@
-#include<stdio.h>
-/*
-*Write a function that prints a string, followed by a new line.
-*Prototype: void _puts_recursion(char *s);
-*/
-
-void _puts_recursion(char *s);
+#include "main.h"
+/**
+ * _puts_recursion - function like puts();
+ * @s: input
+ * Return: Always 0 (Success)
+ */
+void _puts_recursion(char *s)
 {
+	if (*s)
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
 
-char *s;
-if(*s == '\n')
-
-{
-_putchar('\n')
-return 0;
-
+	else
+		_putchar('\n');
 }
-
-_putchar(*s);
-s++;
-_puts_recursion(s);
-}
-
-
 
