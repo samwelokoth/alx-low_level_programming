a16033656ed2721d89744538985efc0cca45d6ee
samwelokoth <samwelokoth8@gmail.com>
2023-08-30 21:17:02 +0300

    main.h

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,37 @@
+#ifndef MAIN_H
+#define MAIN_H
+/**
+*main - header file for protoypes
+*void protoypes (void)
+*int protoypes (int)
+*/
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void reset_to_98(int *n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+void print_number(int n);
+void _puts(char *str);
+int _putchar(char);
+int _isupper(int);
+int _isdigit(int);
+int mul(int, int);
+void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int is_prime_number(int n);
+int is_palindrome(char *s);
+int wildcmp(char *s1, char *s2);
+
+
+
+
+
+
+#endif /* MAIN_H */
