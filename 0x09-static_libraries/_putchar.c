ef380e879dc0b4c879df6237f3f483db0ef216e4
samwelokoth <samwelokoth8@gmail.com>
2023-09-04 00:20:33 +0300

    A library is not a luxury but one of the necessities of life

--- 0x09-static_libraries/_putchar.c
+++ 0x09-static_libraries/_putchar.c
@@ -1,15 +1,14 @@
-#include <main.h>
-#include<stdio.h>
+#include <unistd.h>
+
 
 /**
- * _putchar
- * return 0;
-*/
-
-int _putchar(char c);
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
 {
-
-return write(1,&c ,1); //return a single character to the standard output
-
-
+	return (write(1, &c, 1));
 }
--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -21,7 +21,6 @@ char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
-void _strpbrk(const char *s, const char *locate[]) {
 char *_strstr(char *haystack, char *needle);
-
+char *_strpbrk(char *s, char *accept);
 #endif /* MAIN_H */
