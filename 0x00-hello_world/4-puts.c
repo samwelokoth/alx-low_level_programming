55803e3c81426b7dc2f90fc51915805d3006f463
samwelokoth <samwelokoth8@gmail.com>
2024-05-07 05:39:16 -0400

    Hello , puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+/**
+ * @brief Prints a string followed by a newline.
+ *
+ * This function prints the given string followed by a newline character.
+ *
+ * @return Always 0 (Success).
+ */
 int main(void)
 {
     puts("Programming is like building a multilingual puzzle");
