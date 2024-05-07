1e5304e8c10caa7502f5253d01baf3415d36c911
samwelokoth <samwelokoth8@gmail.com>
2024-05-07 05:31:53 -0400

    Hello , puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,7 @@
 #include <stdio.h>
 
-/**
- * @brief Entry point of the program.
- *
- * This function is the entry point of the program and is automatically
- * called when the program is executed. It prints the phrase
- * "Programming is like building a multilingual puzzle" and returns 0.
- *
- * @return The exit status of the program (0).
- */
-int main() {
-    puts("\"Programming is like building a multilingual puzzle\n");
-    return 0;
+int main(void)
+{
+    puts("Programming is like building a multilingual puzzle");
+    return (0);
 }
-
