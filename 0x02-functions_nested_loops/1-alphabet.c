8f33fbb228999090039643c48048eea40bd317d9
samwelokoth <samwelokoth8@gmail.com>
2024-05-07 18:35:31 -0400

    added some few fixes

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,19 @@
 #include "main.h"
+#include <unistd.h>
+
+/**
+ * print_alphabet - Entry point
+ * Return:0
+ */
 
 void print_alphabet(void)
 {
 char c = 'a';
-while (c <= 'z') 
+while (c <= 'z')
 {
-putchar (c);
+_putchar(c);
 c++;
-if (c <= 'z') 
-{
-putchar (' ');
 }
+_putchar('\n');
 }
-putchar ('\n');
-}
+
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,39 +1,43 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+/* _putchar.c */
+int _putchar(char);
 
-
-int main(void);
-
+/* 1-alphabet.c */
 void print_alphabet(void);
 
+/* 2-print_alphabet_x10.c */
 void print_alphabet_x10(void);
 
-int _islower(int c);
-int _isalpha(int c);
-int print_sign(int n);
+/* 3-islower.c */
+int _islower(int);
 
-int _abs(int n);
+/* 4-alpha.c */
+int _isalpha(int);
 
-int print_last_digit(int n);
+/* 5-sign.c */
+int print_sign(int);
 
+/* 6-abs.c */
+int _abs(int);
+
+/* 7-print_last_digit.c */
+int print_last_digit(int);
+
+/* 8-24_hours.c */
 void jack_bauer(void);
 
-void print_to_98(int n);
+/* 9-times_table.c */
+void times_table(void);
 
-void print_times_table(int n);
+/* 10-add.c */
+int add(int, int);
 
-int sum_multiples_3_5(void);
+/* 11-print_to_98.c */
+void print_to_98(int);
 
-int print_fibonacci(void);
+/* 100-times_table.c*/
+void print_times_table(int);
 
-int sum_even_fibonacci(void);
-int numLength(int num);
-
-
-int print_fibonacci_98(void);
-
-#endif /* MAIN_H*/ 
+#endif
