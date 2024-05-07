d2c2a6606084a9131ee86e86ee6783dd46492ddb
samwelokoth <samwelokoth8@gmail.com>
2024-05-07 18:03:51 -0400

    added some few fixes

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,40 +1,39 @@
-
 #ifndef MAIN_H
 #define MAIN_H
 
-// Standard Library Headers
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
-#include <string.h>
-#include <ctype.h>
+#include <time.h>
 
-// Macros
-#define UNUSED(x) (void)(x)
-#define MAX(a, b) ((a) > (b) ? (a) : (b))
-#define MIN(a, b) ((a) < (b) ? (a) : (b))
 
-// Typedefs
-typedef unsigned int uint;
-typedef unsigned long ulong;
+int main(void);
 
-// Function Prototypes
-void print_hello(void);
-int add_numbers(int a, int b);
-void swap(int *a, int *b);
-
-#endif /* MAIN_H */
-
-int _putchar(char c);
 void print_alphabet(void);
+
 void print_alphabet_x10(void);
+
 int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
-int _abs(int);
-int print_last_digit(int);
+
+int _abs(int n);
+
+int print_last_digit(int n);
+
 void jack_bauer(void);
-void times_table(void);
-int add(int, int);
+
 void print_to_98(int n);
+
 void print_times_table(int n);
+
+int sum_multiples_3_5(void);
+
+int print_fibonacci(void);
+
+int sum_even_fibonacci(void);
+int numLength(int num);
+
+
+int print_fibonacci_98(void);
+
+#endif /* MAIN_H*/ 
