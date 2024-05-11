87cecafc7881924c2a9c6ad034f1c035238cfce9
samwelokoth <samwelokoth8@gmail.com>
2024-05-11 06:58:35 -0400

    Redone the tasks

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,7 +1,6 @@
-IN_H
-
-#include <stdio.h>
+#ifndef MAIN_H
 
 void positive_or_negative(int i);
-
+int convert_day(int month, int day);
+void print_remaining_days(int month, int day, int year);
 #endif /* MAIN_H */
