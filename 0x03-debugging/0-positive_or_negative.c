ae7d4441a685efd5fa5bd9dadd2ce094cbc34113
samwelokoth <samwelokoth8@gmail.com>
2024-05-11 07:23:55 -0400

    Redone the tasks

--- 0x03-debugging/0-positive_or_negative.c
+++ 0x03-debugging/0-positive_or_negative.c
@@ -1,16 +1,14 @@
 #include <stdio.h>
-
 /**
 * positive_or_negative - prints the sign of a number
 * @n: the number to check
 */
-
 void positive_or_negative(int n)
 {
-    if ((n % 10) > 5)
-        printf("greater than 5\n");
-    else if ((n % 10) == 0)
-        printf("0\n");
-    else
-        printf("less than 6 and not 0\n");
+if ((n % 10) > 5)
+printf("greater than 5\n");
+else if ((n % 10) == 0)
+printf("0\n");
+else
+printf("less than 6 and not 0\n");
 }
